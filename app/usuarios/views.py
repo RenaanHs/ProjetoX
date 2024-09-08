ea3ef8f6bfeef8d848156758057d3cda635686cd
Leonardocsp <leonardocesar1605@gmail.com>
2024-09-08 18:14:40 -0300

    ADD: Tudo

---
+++ app/usuarios/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Usuario
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.senha = request.POST.get('senha')
+    novo_usuario.save()
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request,'usuarios.html', usuarios)
